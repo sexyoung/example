9a835c585224ba0ed1f9a36c6f9ce4a1373bb049
Ben Xie <ben.xie@xuenn.com>
2020-01-10 17:54:22 +0800

    小數點

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Comp2 from './Comp2';
 
 export default function App() {
   const [ value, setValue ] = useState(null);
+  const [ value2, setValue2 ] = useState('');
+
   const handleClick = v => {
     setValue(v);
   }
+
+  const handleChange = (limit, { target }) => {
+    const relimitValue = +(+target.value).toFixed(limit);
+    setValue2(
+      relimitValue !== +target.value ?
+      relimitValue:
+      target.value
+    );
+  }
+
   return (
     <div className="App">
       <Comp1 handleClick={handleClick} />
       <Comp2 value={value} />
+
+      <input type="number" value={value2} onChange={handleChange.bind(this, 2)} />
     </div>
   );
 }
